65af7374ca90838321d34fe91a754e1a3b9f97d8
Sergey Pozharitsky <pozhar511@gmail.com>
2021-07-07 20:53:09 +0300

    feat: created footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,12 +1,17 @@
 import { FooterProps } from './Footer.props';
 import styles from './Footer.module.css';
 import cn from 'classnames';
+import { format } from 'date-fns';
 
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
 	return (
-		<div {...props}>
-			Footer
-		</div>
+		<footer className={cn(className, styles.footer)} {...props}>
+			<div>
+				OwlTop 2020 - {format(new Date, 'yyyy')} All rigth reserved
+			</div>
+			<a href="#" target='blank'> Polzovatelskoe soglashenie</a>
+			<a href="#" target='blank'> Policy confendicial</a>
+		</footer>
 	);
 };
--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -2,22 +2,30 @@ import { LayoutProps } from './Layout.props';
 import styles from './Layout.module.css';
 import cn from 'classnames';
 import { Header } from './Header/Header';
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { Sidebar } from './Sidebar/Sidebar';
 import { Footer } from './Footer/Footer';
 
 
 export const Layout = ({ children }: LayoutProps): JSX.Element => {
 	return (
-		<>
-			<Header />
-			<div>
-				<Sidebar />
-				<div>
-					{children}
-				</div>
+		<div className={styles.wrapper}>
+			<Header className={styles.header} />
+			<Sidebar className={styles.sidebar} />
+			<div className={styles.body} >
+				{children}
 			</div>
-			<Footer />
-		</>
+			<Footer className={styles.footer} />
+		</div>
 	);
+};
+
+export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+	return function withLayoutComponent(props: T): JSX.Element {
+		return (
+			<Layout>
+				<Component {...props} />
+			</Layout>
+		);
+	};
 };
